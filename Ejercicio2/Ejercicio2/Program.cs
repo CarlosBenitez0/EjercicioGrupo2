18bf5a5c952e73858e84c59f340e418a8b3d80b0
CarlosBenitez <78523221+0CarlosBenitez0@users.noreply.github.com>
2021-05-04 22:28:17 -0600

    Ejercicio 2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean continuar = true;
+
+            while (continuar.Equals(true)) {
+                Console.Clear();
+                Console.WriteLine("Ingrese su nombre: ");
+                String nombre = Console.ReadLine();
+
+                Console.WriteLine("\nIngrese su edad: ");
+                int edad = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nIngrese su sexo(Mujer=M, Hombre=H): ");
+                String sexo = Console.ReadLine();
+
+                if (sexo == "M" || sexo == "m")
+                {
+                    Console.WriteLine("\nLa Sra." + nombre + " ha sido registrada");
+
+                }
+                else if (sexo == "H" || sexo == "h")
+                {
+                    Console.WriteLine("\nEl Sr." + nombre + " ha sido registrado");
+
+                }
+
+                Console.WriteLine("\n¿Desea continuar? (s/n)");
+                String decision = Console.ReadLine();
+
+                if (decision == "S" || decision == "s")
+                {
+                    continuar = true;
+
+                }
+                else if (decision == "N" || decision == "n")
+                {
+                    continuar = false;
+                    Environment.Exit(0);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
